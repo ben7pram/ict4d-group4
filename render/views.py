301ca960193b30144e4e008a85f4f1b6a2a80a1a
ben7pram <pramod.bendigeri@gmail.com>
2023-04-29 22:28:20 +0200

    simple test

--- render/views.py
+++ render/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'render/index.html', {})
+
+def report(request):
+    return render(request,'render/report.html')
