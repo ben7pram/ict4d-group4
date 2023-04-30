c6df7f007be9c8447abeb45d96f4be26ab460ed4
ben7pram <pramod.bendigeri@gmail.com>
2023-04-30 12:11:53 +0200

    update

--- render/views.py
+++ render/views.py
@@ -9,8 +9,12 @@ def index(request):
     return render(request, 'render/index.html', {})
 
 def report(request):
+    print("inside report----")
     wtype= request.GET['weather_type']
+    print("wtype=",wtype)
     now = datetime.datetime.now()
+    print("now=",now)
     w = WeatherReport(Reporting_Time=now, Reported_Weather_Type=wtype)
+    print("w=",w)
     w.save()
     return render(request,'render/report.html')
