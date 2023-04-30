255b8f13def16fbc9591b08bdc7127535aaacb3c
ben7pram <pramod.bendigeri@gmail.com>
2023-04-30 11:38:51 +0200

    updates

--- render/models.py
+++ render/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class WeatherReport(models.Model):
+    Reporting_Time = models.DateTimeField()
+    Reported_Weather_Type = models.TextField()
+
+    class Meta:
+        managed = False
+        db_table = "Weather_Report"
+        
--- render/views.py
+++ render/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from render.models import WeatherReport
+import datetime
+
+
 
 # Create your views here.
 def index(request):
     return render(request, 'render/index.html', {})
 
 def report(request):
+    wtype= request.GET['weather_type']
+    now = datetime.datetime.now()
+    w = WeatherReport(Reporting_Time=now, Reported_Weather_Type=wtype)
+    w.save()
     return render(request,'render/report.html')
