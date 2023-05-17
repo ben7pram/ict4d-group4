26fbe86b2c05e0db0f744fb31f2d39289e1153f0
ben7pram <pramod.bendigeri@gmail.com>
2023-05-17 14:01:08 +0200

    update

--- render/urls.py
+++ render/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('report_fr/', views.report_fr, name='report_fr'),
     path('weather_info/',views.weather_info,name='weather_info'),
     path('crop_info/',views.crop_info,name='crop_info'),
+    path('crop_info_today/',views.crop_info_today,name='crop_info_today'),
     path('update_weather_type/<int:id>/', views.update_weather_type, name='update_weather_type'),
     path('update_seed_day/<int:id>/', views.update_seed_day, name='update_seed_day'),
 ]
